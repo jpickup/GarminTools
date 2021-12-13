e2cb9b3c52e56bf00b93cb650f573056103b9a74
john <home@johnpickup.com>
2021-12-13 14:15:43 +0000

    Handle routes with no elevation data and no names in the track / route portion of the GPX

--- src/com/johnpickup/garmin/converter/CourseConverter.java
+++ src/com/johnpickup/garmin/converter/CourseConverter.java
@@ -9,19 +9,21 @@ import java.util.List;
 
 public class CourseConverter {
     public Course convert(GpxType gpx) {
-        if (gpx.getRte().size() > 0) {
-            return convertRoute(gpx.getRte().get(0));
-        }
+        String alternativeName = gpx.getMetadata().getName();
 
         if (gpx.getTrk().size() > 0) {
-            return convertTrack(gpx.getTrk().get(0));
+            return convertTrack(gpx.getTrk().get(0), alternativeName);
+        }
+
+        if (gpx.getRte().size() > 0) {
+            return convertRoute(gpx.getRte().get(0), alternativeName);
         }
 
         throw new RuntimeException("GPX doesn't contain a route or a track, nothing to convert");
     }
 
-    private Course convertTrack(TrkType trkType) {
-        String name = trkType.getName();
+    private Course convertTrack(TrkType trkType, String alternativeName) {
+        String name = trkType.getName()==null || trkType.getName().isEmpty()? alternativeName : trkType.getName();
         List<CoursePoint> points = new ArrayList<>();
         for (TrksegType trkSeg : trkType.getTrkseg()) {
             for (WptType wptType : trkSeg.getTrkpt()) {
@@ -31,8 +33,8 @@ public class CourseConverter {
         return new Course(name, points);
     }
 
-    private Course convertRoute(RteType rteType) {
-        String name = rteType.getName();
+    private Course convertRoute(RteType rteType, String alternativeName) {
+        String name = rteType.getName() == null || rteType.getName().isEmpty() ? alternativeName : rteType.getName();
         List<CoursePoint> points = new ArrayList<>();
         for (WptType wptType : rteType.getRtept()) {
             points.add(wayPointToCoursePoint(wptType));
@@ -44,6 +46,6 @@ public class CourseConverter {
         return new CoursePoint(
                 Math.toRadians(wptType.getLat().doubleValue()),
                 Math.toRadians(wptType.getLon().doubleValue()),
-                wptType.getEle().doubleValue());
+                wptType.getEle()==null?0d:wptType.getEle().doubleValue());
     }
 }
