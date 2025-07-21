6ff8f1700eb74e6ccc039148580599b532f8afc3
John Pickup <home@johnpickup.com>
2025-07-21 07:46:14 +0100

    Add uncaught exception handler

--- app/src/main/java/com/johnpickup/app/javafx/AppLauncher.java
+++ app/src/main/java/com/johnpickup/app/javafx/AppLauncher.java
@@ -1,7 +1,13 @@
 package com.johnpickup.app.javafx;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class AppLauncher {
+    private static final Logger log = LoggerFactory.getLogger(AppLauncher.class);
+
     public static void main(String[] args) {
+        Thread.setDefaultUncaughtExceptionHandler((t, e) -> log.error("Unhandled error", e));
         MainForm.main(args);
     }
 }
